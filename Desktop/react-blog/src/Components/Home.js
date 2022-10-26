0769d5478409ce2ae6c3793a75b222261906c2df
MathurJi <anileshjjssacademy84@gmail.com>
2022-10-26 10:29:04 +0530

    add the new style

--- Desktop/react-blog/src/Components/Home.js
+++ Desktop/react-blog/src/Components/Home.js
@@ -1,6 +1,25 @@
 import { useEffect, useState} from 'react';
 import {firestore} from '../firebase';
 import {Link} from 'react-router-dom';
+import styled from 'styled-components';
+
+const BlogHeading =styled.h1`
+text-align: center;
+color: #2196f3;
+margin-bottom: 2px;
+`;
+
+const PostSubtitle=styled.p`
+font-size: 13px;
+`;
+
+
+const Post=styled.div`
+border: 1px solid #e1e1e1;
+padding: 10px 10px;
+border-radius: 5px;
+margin-top: 10px;
+`;
 function Home() {
     const [posts,setPosts] =useState([]);
 
@@ -21,17 +40,17 @@ function Home() {
     },[]);
     return (
     <div className="home">
-        <h1 style={styles.heading}>Tech blog</h1>
+        <BlogHeading>Tech blog</BlogHeading>
         <div id="blog-by">Anilesh</div>
 
 
         {posts.map((post,index)=>(
-            <div className='post' key ={`post-${index}`}>
+            <Post className='post' key ={`post-${index}`}>
                 <Link to={`/post/${post.id}`}>
                     <h3>{post.title}</h3>
                 </Link>
-                <p>{post.subTitle}</p>
-            </div>
+                <PostSubtitle>{post.subTitle}</PostSubtitle>
+            </Post>
         ))}
     </div>
     );
